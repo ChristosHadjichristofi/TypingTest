103c723ffc80bcaa89fb138471d5d0c3d2319122
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2022-01-02 17:33:52 +0200

    Complete landing page styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 import styles from "./Landing.module.css";
 import flash from "./../../assets/hero.png";
 
 const Landing = () => {
     return (
         <div className={styles.landing_container}>
-            <div className={styles.landing_left}>
+            <div data-aos="fade-right" className={styles.landing_left}>
                 <h1 className={styles.landing_header}>Can you type...</h1>
                 <div className={styles.typewriter_container}>
-                    <p>Fast?</p>
-                    <p>Correct?</p>
-                    <p>Quick?</p>
+                <Typewriter
+                    options={{
+                        strings: ['Fast?', 'Correct?', 'Quick?'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
                 </div>
             </div>
             <div className={styles.landing_right}>
-                <img className={styles.flashimg} src={flash} alt="hero"></img>
+                <img 
+                    data-aos="fade-left" 
+                    className={styles.flashimg} 
+                    src={flash} 
+                    alt="hero">
+                </img>
             </div>
         </div>
     );
